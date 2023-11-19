2c77e489d1479339915efa1bb70bd09005ef0328
Ashraf Mohammed Mohammed Hofney <am.hofney@gmail.com>
2023-11-20 01:31:15 +0200

    Update Services.js

--- src/components/Services.js
+++ src/components/Services.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import img from '../images/web.svg'; 
 import img2 from '../images/app.svg';
-import img3 from '../images/hosting.svg';
 import img4 from '../images/consultation.svg';
 
 const Services = () => {
